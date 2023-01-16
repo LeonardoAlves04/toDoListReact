a0d8b6d752e797a61012a5f18c3a78e872bfca8c
Leonardo Alves <leonardoalves201510@gmail.com>
2023-01-16 10:54:27 -0300

    fix(): arrumando erro da imagem do botão não aparecendo

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,22 +3,22 @@ import Card from './Card'
 
 function DoneImg(props) {
     if (props.done) {
-        return (<img src="toDoListReact/images/s.png" alt='done'></img>)
+        return (<img src="./images/done.png" alt='done'></img>)
     } else {
-        return (<img src="toDoListReact/images/n.png" alt='undone'></img>)
+        return (<img src="./images/undone.png" alt='undone'></img>)
     }
 }
 
 function ListItem(props) {
     return (<li>
-    <Card className={props.item.done ? "done item" : "item"}> 
-    {props.item.text}
+        <Card className={props.item.done ? "done item" : "item"}>
+            {props.item.text}
             <div>
-                <button onClick={() => { props.onDone(props.item) }}><DoneImg done={props.item.done}></DoneImg></button>
-                <button className="deleteButton" onClick={() => { props.onItemDeleted(props.item) }}><img src="toDoListReact/images/delete.png" alt='delete'></img></button>
+                <button className="doneButton" onClick={() => { props.onDone(props.item) }}><DoneImg done={props.item.done}></DoneImg></button>
+                <button className="deleteButton" onClick={() => { props.onItemDeleted(props.item) }}><img src="./images/delete.png" alt='delete'></img></button>
             </div>
-            </Card>
-        </li>)
+        </Card>
+    </li>)
 }
 
 export default ListItem;
